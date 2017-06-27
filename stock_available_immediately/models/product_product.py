2b46ebb6fac137a1ce759c632dae2018db339456
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-06-27 21:04:17 +0200

    [CHG] optimize stock computation by avoiding to call useless compute

--- stock_available_immediately/models/product_product.py
+++ stock_available_immediately/models/product_product.py
@@ -3,18 +3,15 @@
 # Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    def _product_available(self, field_names=None, arg=False):
-        res = super(ProductProduct, self)._product_available(
-            field_names=field_names, arg=arg)
-        for prod_id in res:
-            res[prod_id]['immediately_usable_qty'] =\
-                res[prod_id]['immediately_usable_qty'] - \
-                res[prod_id]['incoming_qty']
-
-        return res
+    @api.multi
+    @api.depends('virtual_available', 'incoming_qty')
+    def _compute_immediately_usable_qty(self):
+        super(ProductProduct, self)._compute_immediately_usable_qty()
+        for prod in self:
+            prod.immediately_usable_qty -= prod.incoming_qty
